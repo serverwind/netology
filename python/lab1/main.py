a8230420898b6480baa9d362396a85dde994472b
Lestrangeqq <lestrangeqq@gmail.com>
2022-10-14 20:03:03 +0300

    Add solution for first lab

---
+++ python/lab1/main.py
@@ -0,0 +1,20 @@
+dict = {
+	
+}
+
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+
+dict[date] = task
+
+date2 = input('Введите дату: ')
+task2 = input('Введите задачу: ')
+
+dict[date2] = task2
+
+date3 = input('Введите дату: ')
+task3 = input('Введите задачу: ')
+
+dict[date3] = task3
+
+print(dict)
